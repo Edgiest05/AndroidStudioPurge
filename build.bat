d8c07cff1dcf9d4cdeae057b4a20385715429aa0
Edgiest05 <mattiagerosa05@gmail.com>
2023-11-17 17:14:28 +0100

    Removed deprecated building options

--- build.bat
+++
@@ -1,5 +0,0 @@
-@set OUTNAME=%~dp0build\ASPurge
-@set CFLAGS=-ggdb -Wall -Wextra -std=c99 -L %~dp0lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
-
-@set COMPILE=%~dp0src/main.c -o %OUTNAME% %CFLAGS%
-gcc %COMPILE%
